1f7299e9f850b47919b35ecec28a81a15566eeb8
Shoeb Raza <razashoeb46@gmail.com>
2025-02-11 23:25:35 +0530

    Add files via upload

---
+++ BubbleSort.py
@@ -0,0 +1,9 @@
+aList = [15, 6, 13, 22, 3, 52, 2]
+print("Original List : ", aList)
+n = len(aList)
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if aList[j] > aList[j + 1]:
+            aList[j], aList[j + 1] = aList[j + 1], aList[j]
+print("List After Sorting : ", aList)
+
