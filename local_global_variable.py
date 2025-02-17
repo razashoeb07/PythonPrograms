36a7c1e805b7068b9fe7f8e693ba4a7098d692ef
Shoeb Raza <razashoeb46@gmail.com>
2025-02-17 20:41:02 +0530

    Add files via upload

---
+++ local_global_variable.py
@@ -0,0 +1,22 @@
+def my_func():   # Local Variable Example
+    local_val = 10
+    print(local_val)
+
+
+my_func()
+# print(local_var) # NameError: name 'local_var' is not defined
+
+"""
+global_var = 20  # Global Variable Example
+
+
+def my_func():
+    global global_var
+    global_var = 50  # modify global variable
+    # print(global_var)
+
+
+my_func()
+print(global_var)
+
+"""
